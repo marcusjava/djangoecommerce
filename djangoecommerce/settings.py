452a81e95346f801a8edb3819e9ad303a11daac6
marcusjava <buba.sergipe@gmail.com>
2016-09-13 15:51:27 -0300

    mudancas no settings

--- djangoecommerce/settings.py
+++ djangoecommerce/settings.py
@@ -123,12 +123,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
